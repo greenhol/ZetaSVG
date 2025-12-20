b1c9e89e65825f2bd5af7a5bd7b6ef645f84ce8b
Achim Gruen <achim.gruen@gmail.com>
2025-12-20 20:22:31 +0100

    fix: hash not to fill up browser history

--- src/utils/url-handler.ts
+++ src/utils/url-handler.ts
@@ -3,13 +3,15 @@ export class UrlHandler {
     private readonly worldIdParameter: string = 'worldId';
 
     getWorldId(): number | null {
+        if (!window.location.hash) return null;
         const hash = window.location.hash.substring(1);
         const parameters = new URLSearchParams(hash);
         return this.parseIntOrNull(parameters.get(this.worldIdParameter));
     }
 
     updateWorldId(id: number) {
-        window.location.hash = `${this.worldIdParameter}=${encodeURIComponent(id)}`;
+        const newHash = `${this.worldIdParameter}=${encodeURIComponent(id)}`;
+        window.history.replaceState(null, "", `#${newHash}`);
     }
 
     private parseIntOrNull(value: string | null): number | null {
