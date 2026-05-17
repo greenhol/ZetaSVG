7799b544136fa7bf191ebbf0e83c46bbd6860b3c
Achim Gruen <achim.gruen@gmail.com>
2026-05-17 20:08:16 +0200

    build: development build

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,59 @@
-const path = require('path')
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const packageJson = require('./package.json');
 
-module.exports = {
-    entry: './src/index.ts',
-    mode: 'production',
-    // mode: 'development',
-    // devtool: 'eval',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            APP_VERSION: JSON.stringify(packageJson.version),
-            APP_NAME: JSON.stringify(packageJson.description),
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            version: packageJson.version,
-            name: packageJson.description,
-            inject: false,
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/favicon.ico', to: 'favicon.ico' },
-                { from: 'src/virtual-keyboard.html', to: 'virtual-keyboard.html' },
-                { from: 'src/index.css', to: 'index.css' },
-                { from: 'shared/config/ui/config-overlay.html', to: 'config-overlay.html' },
-            ],
-        }),
-    ],
-}
+module.exports = (_, argv) => {
+    const isProd = argv.mode === 'production';
+
+    return {
+        entry: './src/index.ts',
+        mode: argv.mode,
+
+        devtool: isProd ? false : 'eval-source-map',
+
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                }
+            ]
+        },
+
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+            clean: true,
+        },
+
+        optimization: isProd ? {} : { minimize: false },
+
+        plugins: [
+            new webpack.DefinePlugin({
+                APP_VERSION: JSON.stringify(packageJson.version),
+                APP_NAME: JSON.stringify(packageJson.description),
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                version: packageJson.version,
+                name: packageJson.description,
+                inject: false,
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'src/favicon.ico', to: 'favicon.ico' },
+                    { from: 'src/virtual-keyboard.html', to: 'virtual-keyboard.html' },
+                    { from: 'src/index.css', to: 'index.css' },
+                    { from: 'shared/config/ui/config-overlay.html', to: 'config-overlay.html' },
+                ],
+            }),
+        ],
+    };
+};
